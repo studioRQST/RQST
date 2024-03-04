6294c5ade9e8c5259109d55050911d634c57da95
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-03-04 14:25:34 +0100

    section-three

--- components/home/SectionThree.vue
+++ components/home/SectionThree.vue
@@ -3,9 +3,25 @@
 </script>
 
 <template>
-
+  <section>
+  <h3>CASES</h3>
+  <p>Ontdek ons werk</p>
+  <button>Bekijk meer cases</button>
+  </section>
 </template>
 
 <style scoped>
+section {
+  margin: 2rem 2rem 0 2rem;
+  padding: 2rem 0 0 3rem;
+}
+
+h3 {
+  font-size: 7rem;
+}
+
+p {
+  font-size: 1.2rem;
+}
 
 </style>
