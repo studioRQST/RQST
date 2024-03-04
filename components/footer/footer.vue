5b5f8b88298b3e93fde33f4a8248122b4412d67d
rosahoffer <rosahoffer@live.nl>
2024-03-04 14:24:58 +0100

    push footer

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -3,6 +3,7 @@
         <ul>
             <li><p>© Studio RQST</p></li>
             <li><a>Algemene voorwaarden</a></li>
+            <a href="https://www.instagram.com/studio.rqst"></a>
         </ul>
     </footer>
 </template>
