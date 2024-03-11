91a32136a45bf04d4d0655e081a7e79df0861823
rosahoffer <rosahoffer@live.nl>
2024-03-11 15:42:14 +0100

    contactpage push

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
-
-<!--HTML -->
 <template>
   <link rel="stylesheet" href="https://use.typekit.net/eyu4tao.css">
   <div>
     <PageLoader/>
-  <NuxtLayout>
-    <NuxtLink to="home-page" />
-    <NuxtPage/>
-  </NuxtLayout>
+      <NuxtLayout>
+        <NuxtLink to="home-page" />
+        <NuxtPage/>
+      </NuxtLayout>
   </div>
 </template>
 
