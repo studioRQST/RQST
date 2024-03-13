c27c5f011eb554ed1cd50cdc46ce8ef8fe620aea
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-03-13 13:57:51 +0100

    succes-page-form
    simpele css styling

--- pages/succes-page-form.vue
+++ pages/succes-page-form.vue
@@ -5,7 +5,7 @@
 <template>
   <section>
     <div class="flex-container">
-      <h1> Bedankt voor je RQST, we nemen zo spoedig mogelijk contact met je op!</h1>
+      <h1> Bedankt voor je <span>RQST</span>, we nemen zo <span>spoedig</span> mogelijk contact met je op!</h1>
 
      <nuxt-link class="custom-link" to="/contact">Ga terug</nuxt-link>
     </div>
@@ -16,7 +16,7 @@
 <style scoped>
 section {
   position: relative;
-  top: 50vh;
+
 }
 
 .flex-container {
@@ -24,16 +24,30 @@ section {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background-color: var(--second-background);
+  height: 90vh;
+  margin: 2rem 2rem 2rem 2rem;
+  border-radius: 15px;
 }
 
 h1 {
-  color: var(--teriary-color-yellow);
+  color: var(--secundair-color-l-purple);
+  font-size: 3rem;
+  text-align: center;
+  width: 80%;
+  line-height: 3.2rem;
   margin-bottom: 2rem;
 }
 
+span {
+  color: var(--background-dark-purple);
+}
+
+
 .custom-link {
-  color: var(--teriary-color-yellow);
+  color: var(--background-dark-purple);
   text-decoration: none;
+  font-weight: bold;
 }
 
 </style>
