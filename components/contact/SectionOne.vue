e6c8c79945ed01b461741d29528f4c7120880207
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-03-16 19:50:46 +0100

    Contact section-one responsive

--- components/contact/SectionOne.vue
+++ components/contact/SectionOne.vue
@@ -3,8 +3,10 @@
       <div class="flex-container-text">
         <p class="title-uppercase">Wij staan voor je klaar!</p>
         <h2>Heb je een<span>Request?</span></h2>
-        <p>Bij Studio RQST luisteren we naar jouw behoeften en staan we<span>klaar om je te helpen bij het realiseren van je projecten.</span></p>
+        <p class="p-intro">Bij Studio RQST luisteren we naar jouw behoeften en staan we<span>klaar om je te helpen bij het realiseren van je projecten.</span></p>
+        <div class="scroll-down">
         <ScrollDownYellow />
+        </div>
       </div>
     </section>
 </template>
@@ -12,9 +14,12 @@
   <style scoped>
       
     section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       background-color: var(--second-background);
       margin: 2rem 2rem 0 2rem;
-      padding: 14rem 3rem 0 3rem;
+      padding: 2rem 3rem 0 3rem;
       border-radius: 15px;
       height: 100vh;
     }
@@ -49,7 +54,54 @@
       justify-content: center;
       align-items: center;
       gap: 5rem;
-      margin-top: -2rem;
     }
-  
+
+    .scroll-down {
+      position: relative;
+      top: -2rem;
+    }
+
+    /* Media queries voor verschillende schermformaten */
+    
+    /* LAPTOP */
+    @media screen and (max-width: 80rem) {
+      h2 {
+        font-size: 6rem;
+      }
+    }
+
+    /* LAPTOP */
+    @media screen and (max-width: 60rem) {
+      section {
+        margin: 2rem 2rem 0 2rem;
+      }
+
+
+      h2 {
+        font-size: 5rem;
+      }
+
+      p {
+        font-size: 1rem;
+      }
+    }
+
+    /* MOBILE L */
+    @media screen and (max-width: 30rem) {
+      section {
+        margin: 1rem 1rem 0 1rem;
+      }
+
+      h2 {
+        font-size: 2.9rem;
+      }
+
+      .p-intro {
+        font-size: 1rem;
+      }
+    }
+
+
   </style>
+<script setup lang="ts">
+</script>
