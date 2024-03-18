f97e9e7827feca86e89f13e17bb6fb1b28d755a8
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-03-18 10:51:04 +0100

    footer responsive

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -9,24 +9,36 @@
           <button class="primary-button bold-weight">Stuur je request</button>
         </NuxtLink>
       </div>
-      `
     </section>
+
     <section class="links-contain">
-      <ul>
-        <div>
+      <div class="flex-footer">
+
+        <div class="flex-text">
+        <ul>
           <li><p class="bold-weight">Copyright © 2024 Studio RQST</p></li>
-         <nuxt-link to="terms-conditions"> <li><a class="bold-weight" href="">Algemene voorwaarden</a></li></nuxt-link>
+          <nuxt-link to="terms-conditions">
+            <li><a class="bold-weight" href="">Algemene voorwaarden</a></li>
+          </nuxt-link>
+        </ul>
         </div>
-        <div>
-          <li><a href="https://www.instagram.com/studio.rqst/">
-            <img src="/static/img/InstagramIcon.svg" alt="Instagram Link">
-          </a></li>
-          <li><a href="mailto:info@rqst.studio">
-            <img src="/static/img/MailIcon.svg" alt="Email Link">
-          </a></li>
-        </div>
-      </ul>
+
+          <div class="flex-socials">
+            <ul>
+            <li><a href="https://www.instagram.com/studio.rqst/">
+              <img src="/static/img/InstagramIcon.svg" alt="Instagram Link">
+            </a></li>
+            <li><a href="mailto:info@rqst.studio">
+              <img src="/static/img/MailIcon.svg" alt="Email Link">
+            </a></li>
+            </ul>
+          </div>
+
+
+      </div>
     </section>
+
+
   </footer>
 </template>
 
@@ -65,6 +77,9 @@ span {
   gap: 4rem;
 }
 
+
+
+
 .links-contain {
   background-color: var(--secundair-color-l-purple);
   padding: 2.5rem 2.5rem 2rem 2.5rem;
@@ -73,19 +88,27 @@ span {
   border-radius: 20px;
 }
 
-ul {
+.flex-footer {
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
-  gap: 1rem;
-  list-style-type: none;
-  align-items: center;
 }
 
-div {
+.flex-text ul {
   display: flex;
-  gap: 1rem;
+  flex-direction: row;
+  gap: 2rem;
 }
 
+.flex-socials ul {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+}
+
+ul {
+  list-style-type: none;
+}
 
 li {
   color: var(--background-dark-purple);
@@ -119,40 +142,74 @@ a:focus {
   outline: none;
 }
 
-@media screen and (max-width: 40rem) {
 
+/* Media queries voor verschillende schermformaten */
+
+/* LAPTOP */
+@media screen and (max-width: 80rem) {
+  h2 {
+    font-size: 6rem;
+  }
+
+  .arrow-component {
+    margin-top: -3rem;
+  }
+}
+
+/* TABLET */
+@media screen and (max-width: 60rem) {
+  section {
+    margin: 2rem 2rem 0 2rem;
+  }
+
+
+  h2 {
+    font-size: 5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+}
+
+/* MOBILE L */
+@media screen and (max-width: 30rem) {
   .contact-contain {
-    margin: 0 1rem;
-    padding: 1rem;
-    border-radius: 15px;
-  }
-
-  .contact-contain h2{
-    padding: 0 1rem;
-  }
-
-  .contact-contain h2 span{
-    display: inline;
-  }
-    
-  .contact-text {
-    font-size: 1.2rem;
-    line-height: 1.7rem;
-  }
-
-  .links-contain{
-    margin: 0 1rem;
-    padding: 1rem;
+    margin: 1rem 1rem 0 1rem;
   }
 
   .links-contain {
-    padding: 2rem 2rem 1.5rem 2rem;
-    margin: 1rem;
-    margin-top: -3rem;
-    border-radius: 15px;
+    margin: -2rem 1rem 0 1rem;
   }
 
+  h2 {
+    font-size: 3rem;
+    width: 90vw;
+  }
+
+  p {
+    width: 80vw;
+  }
+
+  .flex-footer {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .flex-text ul {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 1rem;
+  }
+
+  .flex-socials ul {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
 }
+
 
 </style>
 <script setup lang="ts">
